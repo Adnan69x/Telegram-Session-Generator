24caff9c4c521560fc866d90ee4533886dfd0231
Mohammad Azhar Adnan <161193483+MohammadAzharAdnan@users.noreply.github.com>
2024-03-22 16:49:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
+CHANNEL_ID = getenv("CHANNEL_ID", None)
 OWNER_ID = int(getenv("OWNER_ID", 5041639607))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ADNANiTUNE")
