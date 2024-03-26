4f2c731aa19a64e018a79c4f69c53c2b7c75c990
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-03-27 05:29:57 +0600

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -1,15 +1,38 @@
-from pyrogram import filters
-from pyrogram.types import Message
+import logging
 
-from StringGen import Adnany
-from StringGen.utils import add_served_user, keyboard
+def setup_logger():
+    # Create a logger
+    logger = logging.getLogger('StringSessionLogger')
+    logger.setLevel(logging.INFO)
+    
+    # Create a file handler and set the level to INFO
+    file_handler = logging.FileHandler('string_session.log')
+    file_handler.setLevel(logging.INFO)
+    
+    # Create a formatter and set the format for log messages
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    
+    # Add the file handler to the logger
+    logger.addHandler(file_handler)
+    
+    return logger
 
+def log_start_session(message):
+    logger = setup_logger()
+    logger.info(f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}")
 
-@Adnany.on_message(filters.command("start") & filters.private & filters.incoming)
-async def f_start(_, message: Message):
-    await message.reply_text(
-        text=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Adnany.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
-        reply_markup=keyboard,
-        disable_web_page_preview=True,
-    )
-    await add_served_user(message.from_user.id)
+def log_banned_user(message):
+    logger = setup_logger()
+    logger.info(f"User {message.from_user.mention} has been banned. ID: {message.from_user.id}, Username: @{message.from_user.username}")
+
+def main():
+    # Example usage:
+    # message = <get_message_somehow>
+    # log_start_session(message)
+    # or
+    # log_banned_user(message)
+    pass
+
+if __name__ == "__main__":
+    main()
