121cd15f05eb65e11b7f34e92c04d075f13acf60
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-06-16 15:04:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ API_HASH = getenv("04e2f4e414cdabe52ad985adaa6cfe09")
 
 BOT_TOKEN = getenv("7198675016:AAEhGG7AedNnEHXY9J9kceXjtmkMsjBWwdU")
 MONGO_DB_URI = getenv("mongodb+srv://Farhan:FARHAN@cluster0.uqsweah.mongodb.net/?retryWrites=true&w=majority", None)
-OWNER_ID = int(getenv("5041639607", 5041639607))
+OWNER_ID = int(getenv("ONWER_ID", 5041639607))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ADNANiTUNE")
