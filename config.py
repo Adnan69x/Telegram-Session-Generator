1a6ff0952db491206be969acfbaaad60e75ea22e
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-06-16 15:03:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27913018"))
+API_HASH = getenv("04e2f4e414cdabe52ad985adaa6cfe09")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-OWNER_ID = int(getenv("OWNER_ID", 5041639607))
+BOT_TOKEN = getenv("7198675016:AAEhGG7AedNnEHXY9J9kceXjtmkMsjBWwdU")
+MONGO_DB_URI = getenv("mongodb+srv://Farhan:FARHAN@cluster0.uqsweah.mongodb.net/?retryWrites=true&w=majority", None)
+OWNER_ID = int(getenv("5041639607", 5041639607))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ADNANiTUNE")
